e67ef9042bd1f5ad140cffcf6e547bdb727e104a
TRIANA115 <andresfelipetriana443@gmail.com>
2024-12-20 12:49:17 -0500

    se agrego un app.tsx para llamar los models para los diferentes tipos de usuarios  y también se creo un nuevo tsx llamado PaginaDeAdmin para la pagina principal del Administrador y vamos a crear la pagina principal con react y con nextui

---
+++ src/components/App.tsx
@@ -0,0 +1,7 @@
+import { PaginaDeAdmin } from "../models/Administrator/PaginaDeAdmin";
+
+export const App = () => {
+  return (
+    <PaginaDeAdmin />
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { App } from "./components/App";
 
 import "../public/js/tailwind.js";
 
@@ -7,6 +8,6 @@ import "../public/js/tailwind.js";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <App/>
   </StrictMode>
 );
